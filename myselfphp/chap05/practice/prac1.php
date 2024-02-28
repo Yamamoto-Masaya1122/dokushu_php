a21fccee831178a473c44d5e865369d955e1273a
Masaya Yamamoto <masaya1122baseball@gmail.com>
2024-02-28 11:20:56 +0900

    2月28日独習PHP-part05

--- myselfphp/chap05/practice/prac1.php
+++ myselfphp/chap05/practice/prac1.php
@@ -3,8 +3,9 @@ $str = 'サーバーサイド技術'; //一問
 print mb_substr($str, -5, 3);
 echo '<br>';
 
-print mb_convert_kana($str, 'K'); //二問
+$str2 = 'ｻｰﾊﾞｰｻｲﾄﾞ技術';
+print mb_convert_kana($str2, 'KV'); //二問
 echo '<br>';
 
-$str2= '鈴木\t太郎\t男\t50\t広島県'; //三問
-print explode("\t", $str2);
+$str3= '鈴木\t太郎\t男\t50\t広島県'; //三問
+print_r(explode("\t", $str3));
