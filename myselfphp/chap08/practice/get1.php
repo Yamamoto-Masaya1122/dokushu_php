e993bd9bce0f2908b22c95c445367850cf2c359a
Masaya Yamamoto <masaya1122baseball@gmail.com>
2024-03-04 15:59:21 +0900

    3月4日独習PHP-part08

---
+++ myselfphp/chap08/practice/get1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ポストデータ</title>
+</head>
+<body>
+<!-- action属性にはポストデータの送信先を指定-->
+<form method="GET" action="get2.php">
+<label for="name">名前:</label>
+<input id="name" type="text" name="name" size="15" />
+<input type="submit" value="送信" />
+</form>
+</body>
+</html>
---
+++ myselfphp/chap08/practice/redirect.php
@@ -0,0 +1,2 @@
+<?php
+header('Location: http://localhost:8888/myselfphp/chap08/practice/req_headers.php');
