9fb0750293b42c2c7cb35c1f719b383751a390ee
Masaya Yamamoto <masaya1122baseball@gmail.com>
2024-03-09 10:25:02 +0900

    3月9日独習PHP-part11

---
+++ myselfphp/chap11/practice/class_info.php
@@ -0,0 +1,7 @@
+<?php
+#[MyAttribute(Attribute::TARGET_CLASS)]
+class ClassInfo {
+  public function __construct(
+    public string $version = '1.0',
+    public string $description = '') {}
+}
---
+++ myselfphp/chap11/practice/prac1.php
@@ -0,0 +1,5 @@
+<?php
+//11.1
+#[MyAttribute(1)]
+#[SampleAttribute]
+class Person {}
