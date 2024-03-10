9a60d6aa4ba3d85975ef1df9db298935e34005eb
Masaya Yamamoto <masaya1122baseball@gmail.com>
2024-03-10 11:00:48 +0900

    3月10日独習PHP-part11

---
+++ myselfphp/chap11/prac2.php
@@ -0,0 +1,7 @@
+<?php
+// 11章 章末問題
+// 1問 ① クロスサイトスクリプティング ②エスケープ処理 ③ SQLインジェクション ④SQL ⑤ CSRF ⑥ パストラバーサル ⑦ メールヘッダーインジェクション ⑧スパムメール ⑨ セッションハイジャック ⑩ セッションID 11 require_once 12 eval
+
+// 2問 ① ✖️ ② ✖️ ③ ✖️ ④ ✖️ ⑤ ✖️ ⑥ ○ ⑦ ✖️
+
+// 3問 ① ReflectionClass ②DataInterval::class ③ getMethod ④ newInstance ⑤invoke
